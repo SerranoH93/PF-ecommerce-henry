db4a9768148e88baeaf03f194745b3d5f5168b54
David <david.leandro.palomino.rivera@gmail.com>
2024-06-19 21:46:04 -0500

    Rutas Creadas

--- back/src/controllers/productsController.js
+++ back/src/controllers/productsController.js
@@ -12,13 +12,11 @@ const getAllProducts = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Error en la base de datos', error: error.message });
     }
-    
 }
-
 
 const postNewProduct = async (req, res) => {
     try {
-        
+
         //* Se realizan validaciones del formulario
         let validationsCheck = await newProductSchema.safeParseAsync(req.body);
         // console.log(validationsCheck, 'validaciones')
@@ -87,17 +85,32 @@ const deleteProduct = async (req, res) => {
             }});
             res.status(200).json({ message: 'Producto eliminado exitosamente' });
         }    
-
-        
-
-        
     } catch (error) {
         res.status(500).json({ message: 'Error al eliminar el producto', error: error.message });
+    }
+}
+
+
+const editProduct = async (req, res) => {
+    try {
+        res.status(200).json({message: 'products/edit funciona'});
+    } catch (error) {
+        res.status(500).json({ message: 'Error en la base de datos', error: error.message });
+    }
+}
+
+const activeUnactiveProduct = async (req, res) => {
+    try {
+        res.status(200).json({message: 'products/activeUnactive funciona'});
+    } catch (error) {
+        res.status(500).json({ message: 'Error en la base de datos', error: error.message });
     }
 }
 
 module.exports = {
         getAllProducts,
         postNewProduct,
-        deleteProduct
+        deleteProduct,
+        editProduct,
+        activeUnactiveProduct
     };
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const productsRouter = require('./productsRouter')
+const productsRouter = require('./productsRouter');
+const userRouter = require('./userRouter');
+const adminRouter = require('./adminRouter');
+const { getCategories } = require('../controllers/categoriesController');
+const orderRouter = require('./orderRouter');
 
 router.use('/products', productsRouter);
-
+router.use('/categories', getCategories)
+router.use('/admin', adminRouter)
+router.use('/user', userRouter)
+router.use('/order', orderRouter)
 
 module.exports = router;
