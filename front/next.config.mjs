f3e55ce5fd5504b1079a1e727e683327d37d95fe
Agustin <esparta.01.af@gmail.com>
2024-06-24 21:06:42 -0300

    Envio del usuario al back

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',  // Permitir cualquier host
+          },
+          {
+            protocol: 'http',
+            hostname: '**',  // Permitir cualquier host
+          },
+        ],
+      },
+};
 
 export default nextConfig;
