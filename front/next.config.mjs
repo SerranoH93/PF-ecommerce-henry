90c3732c57582d5b9d0be3848cded1c650d98fb9
RNDOSKO <Ricardo.dosko@gmail.com>
2024-06-26 01:41:45 -0300

    configuration and dependencies

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,11 +3,30 @@ const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [{
-            protocol: "https",
-            hostname: "res.cloudinary.com",
-            port: "",
-            pathname: "/**",
-        }],
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "revston.b-cdn.net",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "acdn.mitiendanube.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "ark8.net",
+                port: "",
+                pathname: "/**",
+            }
+        ],
     }
 };
 
