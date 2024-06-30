8858d139b924ec5187a9a29d6538a0745d8ad342
David <david.leandro.palomino.rivera@gmail.com>
2024-06-30 00:12:51 -0500

    addProduct route updated

--- back/src/models/ShoppingCart.js
+++ back/src/models/ShoppingCart.js
@@ -10,6 +10,13 @@ module.exports = (sequelize) => {
         quantity: { 
             type: DataTypes.INTEGER, 
             allowNull: false 
+        },user_id: {
+            type: DataTypes.CHAR,
+            allowNull: false
+        },
+        product_id: {
+            type: DataTypes.CHAR,
+            allowNull: false
         }
     }, {
         paranoid: true
