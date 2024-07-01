304d026aef6388b75f8c22c2e708a9685c28c690
Agustin <esparta.01.af@gmail.com>
2024-07-01 02:37:44 -0300

    Cambios en el envio de datos al Back

--- front/src/components/AddToCart/AddToCart.tsx
+++ front/src/components/AddToCart/AddToCart.tsx
@@ -12,17 +12,28 @@ interface ButtonCartProps {
 
 const AddToCart: FC<ButtonCartProps> = ({ product, quantity, user }) => {
 
+    const userEmail = user?.email
+
     const handleAdd = async () => {
         const link = "http://localhost:3002/order/add"
         if (user) {
 
-            const orden = {
-                product,
-                quantity,
-                user
-            }
-            console.log('Enviando datos:', orden);
+            
             try {
+                const userDb = await axios.get(
+                    `http://localhost:3002/user/by-email?email=${userEmail}`
+                )
+                
+                const user = userDb.data;
+                console.log(user)
+
+                const orden = {
+                    product,
+                    quantity,
+                    user
+                }
+                console.log('Enviando datos:', orden);
+
                 const sendToCart = await axios.post(
                     link, {orden}, {
                         headers: {
@@ -30,10 +41,10 @@ const AddToCart: FC<ButtonCartProps> = ({ product, quantity, user }) => {
                           }
                     }
                 )
-                console.log(product)
-                console.log(quantity)
-                console.log(user)
-                console.log(orden)
+                //console.log(product)
+                //console.log(quantity)
+                //console.log(userId)
+                //console.log(orden)
                 console.log(sendToCart)
             }
             catch{
