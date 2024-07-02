56ad49b37ab1b8a3115a6c57a25feff9dfb65e62
Facundo Sura <facundomesura@gmail.com>
2024-07-02 06:19:45 -0300

    borre la barra de busqueda y los filtros que no usamos

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Link from "next/link"
 import Image from "next/image"
-import SearchBar from '@/components/SearchBar/SearchBar'
 import Logo from "@/assets/Group 8.svg"
 import { useUser } from "@auth0/nextjs-auth0/client"
 import axios from "axios"
@@ -21,8 +20,6 @@ export default function NavBar() {
                         <Image src={Logo} alt="logoModaUrbana" className="h-full w-auto object-contain" />
                     </div>
                 </Link>
-
-                <SearchBar />
 
                 <ul className="flex gap-5 " >
                     <li>
@@ -49,46 +46,6 @@ export default function NavBar() {
                     <li>
                         <Link href='/carrito' className="text-white">
                             Carrito
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="flex w-full p-4" >
-                <ul className="flex gap-8 justify-around w-full ">
-                    <li>
-                        <Link href="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/destacados">
-                            Destacados
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/hombre">
-                            Hombre
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/mujer">
-                            Mujer
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/ninho">
-                            Niño/a
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/accesorios">
-                            Accesorios
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/contacto">
-                            Contacto
                         </Link>
                     </li>
                 </ul>
