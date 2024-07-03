0357532146aa02d0b2861b2e040e6f21422811ac
David <david.leandro.palomino.rivera@gmail.com>
2024-07-02 23:23:24 -0500

    Clear shoppingCart Controller

--- back/src/routes/orderRouter.js
+++ back/src/routes/orderRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { getAllOrders, addProduct, increaceQuantity, deleteOrder } = require('../controllers/ordersControllers');
+const { getAllOrders, addProduct, increaceQuantity, deleteOrder, clearShoppingCart } = require('../controllers/ordersControllers');
 const orderRouter = Router();
 
 orderRouter.get('/', getAllOrders);
 orderRouter.post('/add', addProduct);
 orderRouter.get('/detail', increaceQuantity);
 orderRouter.delete('/delete/:id', deleteOrder);
-
+orderRouter.delete('/deleteAll', clearShoppingCart)
 module.exports = orderRouter;
